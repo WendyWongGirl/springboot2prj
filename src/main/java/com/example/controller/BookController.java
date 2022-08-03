4f5b05ee0c84e4310f3cf54781957697bac7f7ad
wangying@ctdigitech.com <wangying@ctdigitech.com>
2022-08-03 16:58:20 +0800

    修复分页删除bug（最后一页时页码值不对）

--- src/main/java/com/example/controller/BookController.java
+++ src/main/java/com/example/controller/BookController.java
@@ -5,6 +5,8 @@ import com.example.controller.utils.R;
 import com.example.domain.Book;
 import com.example.service.IBookService;
 import com.google.gson.Gson;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,12 +19,14 @@ import java.util.stream.Collectors;
  */
 @RestController
 @RequestMapping("/books")
+@Api(tags = "SSM工程相关接口")
 public class BookController {
 
     @Autowired
     private IBookService bookService;
 
     @PostMapping
+    @ApiOperation(value = "新增接口")
     public R save(@RequestBody Book book) throws IOException {
         //自定义异常：书名=测试测试时，抛出异常
         if ("测试测试".equals(book.getName())) {
@@ -33,18 +37,21 @@ public class BookController {
     }
 
     @PutMapping
+    @ApiOperation(value = "修改接口")
     public R udpate(@RequestBody Book book) {
         boolean flag = bookService.modify(book);
         return new R(flag, book, flag ? "修改成功" : "修改数据失败，请重试！");
     }
 
     @DeleteMapping("{id}")
+    @ApiOperation(value = "根据主键删除接口")
     public R deleteById(@PathVariable Integer id) {
         boolean flag = bookService.removeById(id);
         return new R(flag, flag ? "删除主键{"+id+"}成功" : "删除主键{" + id + "}数据失败，请重试！");
     }
 
     @GetMapping("{id}")
+    @ApiOperation(value = "根据主键查询接口")
     public R queryById(@PathVariable Integer id) {
         Book book = bookService.getById(id);
         if (book != null) {
@@ -55,6 +62,7 @@ public class BookController {
     }
 
     @GetMapping
+    @ApiOperation(value = "全查询接口")
     public R queryList() {
         List<Book> bookList = bookService.list();
 
@@ -72,9 +80,14 @@ public class BookController {
     }
 
     @GetMapping("{currentPage}/{pageSize}")
+    @ApiOperation(value = "分页查询接口")
     public R queryByPage(@PathVariable Integer currentPage, @PathVariable Integer pageSize, Book book) {
         IPage<Book> bookIPage = bookService.queryByPage(currentPage, pageSize, book);
         if (bookIPage != null) {
+            //如果当前页码值大于总页码值，重新执行分页查询，使用最大也没之作为当前页码值
+            if(currentPage > bookIPage.getPages()){
+                bookIPage = bookService.queryByPage((int)bookIPage.getPages(), pageSize, book);
+            }
             return new R(null != bookIPage, bookIPage, "分页查询成功！");
         } else {
             return new R(null != bookIPage, "分页查询失败！");
